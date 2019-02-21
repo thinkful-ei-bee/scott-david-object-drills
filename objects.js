0ae1b204e4cf35587c703c4d7c7df5af75a89ea6
Scott Dowding <scottddowding1279@gmail.com>
2019-02-21 11:36:31 -0500

    added createMyObject function

---
+++ objects.js
@@ -0,0 +1,13 @@
+/* eslint-disable strict */
+function createMyObject () {
+  return {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    hello: function () {
+      return 'hello';
+    }
+  };
+}
+
+//console.log(createMyObject());
